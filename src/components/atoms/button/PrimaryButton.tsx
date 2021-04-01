80cae3522f9171658cc8f952446070e00020f312
Na0yA1031 <kamekameriki@gmail.com>
2021-04-02 04:02:31 +0900

    ログイン昨日の実装

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -3,12 +3,22 @@ import { memo, ReactNode, VFC } from "react";
 
 type Props = {
     children: ReactNode;
+    onClick: () => void;
+    loading?: boolean;
+    disabled?: boolean;
 }
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-    const { children } = props;
+    const { children, onClick, disabled = false, loading = false } = props;
     return (
-        <Button bg='teal.400' color='white' _hover={{ opacity: '0.8' }}>
+        <Button
+            bg='teal.400'
+            color='white'
+            _hover={{ opacity: '0.8' }}
+            onClick={onClick}
+            disabled={disabled || loading}
+            isLoading={loading}
+        >
             {children}
         </Button>
     )
